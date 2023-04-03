8b26f7b723046522eacd3569304418b376638628
florian-savoie <florianpsp303@gmail.com>
2023-04-03 14:14:24 +0200

    maj

--- src/photo.js
+++
@@ -1,9 +0,0 @@
-function Photo(props) {
-    return (
-      <div>
-        <img src={props.src} alt={props.alt} />
-        <p>{props.caption}</p>
-      </div>
-    );
-  }
-  export default Photo;
